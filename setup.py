c77830a192e19f9bf636bf7e1b855d4b39974e1d
wtanaka.com <wtanaka@users.noreply.github.com>
2018-08-10 10:19:56 -0700

    Initial package setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='protopyenum',
+      version='0.1',
+      description='Protocol Buffer Enumerations in Python',
+      url='http://github.com/wtanaka/protopyenum',
+      author='https://wtanaka.com/',
+      license='Apache Software License (http://www.apache.org/licenses/LICENSE-2.0)',
+      packages=['protopyenum'],
+      zip_safe=False)
